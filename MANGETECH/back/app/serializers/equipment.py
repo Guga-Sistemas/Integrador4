4a1d9e4be93555792e0b50c75b476d01b8f5b793
Guga-Sistemas <gustavomp100@gmail.com>
2025-10-21 20:41:07 -0300

    Atualizado 21/10

--- MANGETECH/back/app/serializers/equipment.py
+++ MANGETECH/back/app/serializers/equipment.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from ..models import *
 
 class EquipmentSerializer(serializers.ModelSerializer):
+    from .environment import EnvironmentSerializer
+    from .category import CategorySerializer
+
+    # environment_FK = EnvironmentSerializer()
+    environment_FK = EnvironmentSerializer()
+    category_FK = serializers.SlugRelatedField(slug_field="name",
+                                               read_only=True)
+
+    # category_FK = CategorySerializer()
     class Meta:
         model = Equipment
         fields = '__all__'
